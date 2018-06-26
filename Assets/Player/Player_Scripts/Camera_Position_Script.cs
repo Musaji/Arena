1b213625ec255b67c4156da9a019d0a179dc5810
Musaji <39049820+Musaji@users.noreply.github.com>
2018-06-26 06:23:13 -0700

    Added

---
+++ Assets/Player/Player_Scripts/Camera_Position_Script.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Position_Script : MonoBehaviour
+{
+    public GameObject player;
+    public Transform target;
+    public Vector3 offset;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        //TODO: Transform Position to be that of target.
+        //offset value is global, not local.
+        print(target.position);
+        transform.LookAt(target);
+        transform.position = target.TransformPoint(offset);
+	}
+}
---
+++ Assets/Player/Player_Scripts/Player_Movement_Script.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement_Script : MonoBehaviour
+{
+    public GameObject player;
+    public float forwardVelocity = .2f;
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            print("Forward");
+            transform.Translate(Vector3.forward * forwardVelocity, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            print("Backward");
+            transform.Translate(-Vector3.forward * forwardVelocity, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Left");
+            transform.Translate(Vector3.left * forwardVelocity, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            print("Right");
+            transform.Translate(Vector3.right * forwardVelocity, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            print("Rotate Left");
+            transform.Rotate(-Vector3.up, Space.Self);
+        }
+        if(Input.GetKey(KeyCode.E))
+        {
+            print("Rotate right");
+            transform.Rotate(Vector3.up, Space.Self);
+        }
+    }
+}
